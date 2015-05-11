9e700e9881f6a306b548e6934cc78d0fd1aca2da
abmn614 <dlming@foxmail.com>
2015-05-12 00:07:00 +0800

    Add UserModel Class

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	$UserInfo = M('User')->select();
+    	$this->UserInfo = $UserInfo;
+    	
         $this->display('index');
     }
 }
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,6 +9,17 @@ return array(
     'Public'    => './Public/',
 
     // 模版分隔符
-    'TMPL_L_DELIM'  =>  '{{',            // 模板引擎普通标签开始标记
-    'TMPL_R_DELIM'  =>  '}}',            // 模板引擎普通标签结束标记
+    'TMPL_L_DELIM'  =>  '{ ', // 模板引擎普通标签开始标记
+    'TMPL_R_DELIM'  =>  ' }', // 模板引擎普通标签结束标记
+
+    //数据库配置信息
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_NAME'   => 'iblog', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '', // 密码
+    'DB_PORT'   => 3306, // 端口
+    'DB_PREFIX' => 'ib_', // 数据库表前缀
+    'DB_CHARSET'=> 'utf8', // 字符集
+
 );
